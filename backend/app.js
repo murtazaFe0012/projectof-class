b7756ce89e48ebef0920703d22f0bd8a0ae4c7b8
hadrien <thodrien@gmail.com>
2025-12-16 15:58:18 +0100

    fix get

--- backend/app.js
+++ backend/app.js
@@ -9,23 +9,16 @@ const connection = await mysql.createConnection({
   database: 'eventdb',
 });
 
-await app.get('/', (req, res) => {
-    
-  res.send(get())
+await app.get('/', async (req, res) => {
+    try {
+        const [rows] = await connection.query('SELECT * FROM `roles`');   
+        res.json(rows)
+        return rows;
+    } catch (err) {
+        console.log(err);
+    }
 })
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000')
 })
-
-async function get(){
-    try {
-        const test = await connection.query('SELECT * FROM `roles`');
-
-        console.log(test);
-        return test;
-    } catch (err) {
-        console.log(err);
-        return "fail";
-    }
-}
