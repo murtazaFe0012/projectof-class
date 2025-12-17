6cc52a90ef4568f323c541fb9ca7085a14614be6
Murtaza <murtazafetrat0012@gmail.com>
2025-12-17 16:35:54 +0100

    creating events and other tables withing slug

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,12 @@
 import express from 'express';
-import mysql from 'mysql2/promise';
+import mysql, { raw } from 'mysql2/promise';
+import slugify from 'slugify';
+
+function generateSlug(title, id) {
+    const rawSlug = `${slugify(title, 
+        { lower: true, strict: true, locale: 'fr' })}-tickets-${id}`;
+    return rawSlug;
+}
 
 const app = express()
 app.use(express.json());
@@ -34,7 +41,72 @@ await app.get('/e/:slug', async (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 })
+//here we get all addresses
+await app.get('/addresses', async (req, res) => {
+    try {
+        const [rows] = await connection.query('SELECT * FROM `adresses`');
+        res.json(rows);
+        return rows;
+    } catch (err) {
+        console.log(err);
+    }
+})
 
+//here we create new event using post method
+await app.post('/events', async (req, res) => {
+    try {
+        const {
+             title,
+      dateStart,
+      dateEnd,
+      adresseID,
+      orgranisatorID,
+      categoryID,
+      description,
+      prix,
+      availablePlaces,
+      maxPlaces,
+      image
+        } = req.body;
+        const [result] = await connection.execute(
+            `INSERT INTO events 
+      (title, updateAt, dateStart, dateEnd, adresseID, orgranisatorID, categoryID,
+       description, prix, availablePlaces, maxPlaces, image)
+      VALUES (?, NOW(), ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+      [title, dateStart, dateEnd, adresseID, orgranisatorID, categoryID, description, prix, availablePlaces, maxPlaces, image]);
+
+      const slug = generateSlug(title, result.insertId);//generate slug after inserting to get the id
+    //update the event with the generated slug
+        await connection.execute(
+        `UPDATE events SET slug = ? WHERE id = ?`,
+             [slug, result.insertId]);
+        res.status(201).json({
+            message: 'Event created successfully',
+            id: result.insertId, 
+            slug});
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: 'Failed to create event' });
+    }
+});
+//here we create new address using post method
+await app.post('/addresses', async (req, res) => {
+   const { street, city, postalCode, country, longitude, latitude } = req.body;
+   const [result] = await connection.execute(
+    `INSERT INTO adresses (street, city, postalCode, country, longitude, latitude)
+     VALUES (?, ?, ?, ?, ?, ?)`,
+    [street, city, postalCode, country, longitude, latitude]
+  );
+  res.status(201).json({id: result.insertId, message: 'Address created successfully' });
+});
+//here we create new catagory using post method
+app.post('/categories', async (req, res) => {
+    const { name } = req.body;
+    const [result] = await connection.execute(
+         `INSERT INTO categorys (name) VALUES (?)`,
+    [name]);
+    res.status(201).json({id: result.insertId, message: 'Category created successfully' });
+});
 
 
 app.listen(4000, () => {
